7b8e3d2582d26481e624e41492cf8895839b23c5
uaison <uaison5460@gmail.com>
2019-05-21 14:07:08 +0800

    增加日期格式化方法

--- datejs.js
+++ datejs.js
@@ -80,3 +80,24 @@ Date.prototype.getSeason = Date.getSeason || function() {
     let season = ~~((this.getMonth()+3)/3);
     return season;
 };
+/**
+ * 日期格式化
+ * @type {Function}
+ * @return {String} 格式化后的字符串
+ */
+Date.prototype.format = Date.prototype.format || function (fmt='yyyy-MM-dd HH:mm:ss') {
+  let o = {
+      "M+": this.getMonth() + 1, //月份
+      "d+": this.getDate(), //日
+      "h+": this.getHours(), //小时
+      "m+": this.getMinutes(), //分
+      "s+": this.getSeconds(), //秒
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+      "S": this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
+
